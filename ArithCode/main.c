3c592d0e1a91a325161afbb24162d77cdd7f4cc1
vdkapten <vdkapten@gmail.com>
2015-04-14 21:33:06 +0300

    Z is removed
    No need for the Z

--- ArithCode/main.c
+++ ArithCode/main.c
@@ -3,12 +3,11 @@
 int add(int, int);
 int main()
 {
-    int x,y,z;
+    int x,y;
     printf("Please input values x and y!\n");
     scanf("%d",&x);
     scanf("%d",&y);
-    z = add(x,y);
-    printf("\n\n Your result is : %d",z);
+    printf("\n\n Your result is : %d",add(x,y));
     return 0;
 }
 int add(int x,int y)
