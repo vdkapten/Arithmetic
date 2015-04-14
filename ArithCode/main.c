a672ea44cdc23592d5999ef88ab85d12a993a2ef
toshibauser <skpriviledged@gmail.com>
2015-04-14 22:53:51 +0300

    Product as well
    We should add product as well

--- ArithCode/main.c
+++ ArithCode/main.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 int add(int, int);
+int mul(int, int);
 int main()
 {
     int x,y;
     printf("Please input values x and y!\n");
     scanf("%d",&x);
     scanf("%d",&y);
-    printf("\n\n Your result is : %d",add(x,y));
+    printf("\n\nAddition result is : %d\nProduct result is : %d",add(x,y),mul(x,y));
     return 0;
 }
 int add(int x,int y)
 {
 return (x+y);
 }
+
+int mul(int x,int y)
+{
+return (x*y);
+}
+
